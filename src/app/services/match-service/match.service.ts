bcf59c823785b2ff170d644ccb21e2240a0a2e40
Ricardo <ricardohsmello@gmail.com>
2025-06-30 18:18:35 -0300

    fix rounds

--- src/app/services/match-service/match.service.ts
+++ src/app/services/match-service/match.service.ts
@@ -34,6 +34,6 @@ import { environment } from '../../../environments/environment';
   }
 
   public scoreRound(round: number) {
-    return this.http.put<void>(`${environment.apiUrl}/${round}/score`, {});
+    return this.http.put<void>(`${environment.apiUrl}/rounds/${round}/score`, {});
   }
 }
