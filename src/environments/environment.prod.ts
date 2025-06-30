5daa2a10091a7c22b284ae2d529bd64186868a57
Ricardo <ricardohsmello@gmail.com>
2025-06-30 17:18:06 -0300

    testing

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://g12-football-frontend.onrender.com',
+  apiUrl: 'https://g12-football-backend.onrender.com',
   keycloak: {
     url: 'https://g12-football-keycloak.onrender.com',
     realm: 'g12',
