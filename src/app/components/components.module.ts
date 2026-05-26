a7c1abf82fabb26ee232481c37eed3cf3a50caae
Ricardo <ricardohsmello@gmail.com>
2026-05-26 13:13:35 -0300

    Added charts filter

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { FooterComponent } from './footer/footer.component';
@@ -13,6 +14,7 @@ import {MaterialExampleModule} from "../../material.module";
   imports: [
     CommonModule,
     RouterModule,
+    ReactiveFormsModule,
     NgbModule,
     MaterialExampleModule
   ],
